ed87c1700936ff7fe1c21612767b458d48d623ea
murakami7581 <yoshi7614ya@icloud.com>
2022-09-07 21:24:34 +0900

    sample

--- index.php
+++ index.php
@@ -1,55 +1,5 @@
 <?php get_header(); ?>
 <main class="l-main">
-  <div class="p-hero">
-    <h1>わが子の<span>笑顔</span>は家族の<span>ハッピー<span>に！
-          <br>
-          わが子の<span>自信</span>は家族の<span>ワクワク<span>に！
-    </h1>
-    <div class="c-sns">
-      <a href=" https://twitter.com/sho_nnn2021?ref_src=twsrc%5Etfw"><i class="fa-brands fa-twitter"></i></a>
-      <!-- facebookアカウウント不明なのでhref空欄 -->
-      <a href=""> <i class="fa-brands fa-facebook"></i></a>
-      <!-- instagramアカウウント不明なのでhref空欄 -->
-      <a href=""><i class="fa-brands fa-instagram"></i></a>
-    </div>
-    <div class="p-hero__reg">
-      <h3>まずは安心の無料説明会から→→</h3>
-      <button class="c-line"></button>
-    </div>
-  </div>
-  <article class="p-introduction">
-    <section class="p-card">
-      <h2>オンライン個別サポート塾翔って？</h2>
-      <p>学校生活を楽しむことに特化した
-        <br>
-        オンライン自習型サポート塾です。
-      </p>
-      <button>
-        <a href="">普通の塾と自習型サポート塾の違いは何？</a>
-      </button>
-    </section>
-    <section class="p-card">
-      <h2>いつでもどこでもつながるコミュニティ</h2>
-      <h3>励まし合って高め合える環境</h3>
-      <p>オンライ個別サポート塾翔は学習スペースとして、oViceを採用。オンラインでも相手を可視化して同じ空間で学習している感覚が味わえます。
-        <br>
-        遠くにいても近くに感じるほどよい距離感で高め合いましょう！
-      </p>
-      <button>
-        <a href="">学習の仕組みを詳しく知りたい</a>
-      </button>
-    </section>
-    <section class="p-card">
-      <h2>塾なのに教材がない！？</h2>
-      <h3>普通じゃない！オンライン個別サポート塾翔の特徴</h3>
-      <p>激動の時代を乗り越えるにはどうしたらいいでしょうか？
-        <br>
-        学業？スポーツ？もちろんそれらも大事ですが、一番は「お子さん自身のやる気と継続力」です！
-      </p>
-      <button>
-        <a href="">その仕組みを詳しく知りたい</a>
-      </button>
-    </section>
-  </article>
+
 </main>
 <?php get_footer(); ?>
